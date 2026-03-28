1e6b60f9ab6556f612d20e3f564823a79873a343
MAROUA871 <bouderrazmaroua3@gmail.com>
2026-03-28 15:13:33 +0100

    Add temporary session and DB connection

--- public/index.php
+++ public/index.php
@@ -1,7 +1,13 @@
 <?php
+session_start();
+
+// TEMPORARY FAKE LOGIN (remove later)
+$_SESSION['user_id'] = 1;
+$_SESSION['role'] = 'admin';
+
 echo "University Portal is running<br>";
 
-// Include DB safely
+// Database connection
 include(__DIR__ . "/../config/bd.php");
 
 echo "Database connected!";
